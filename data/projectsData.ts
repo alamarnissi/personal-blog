f89f198974b13273916c32813e025d9cacfa6a41
alamarnissi <ala.marnissi30@gmail.com>
2024-12-26 10:11:19 +0100

    add data in Projects section

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,23 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Acalainer - E-Learning Platform Dedicated to Professionals',
+    description: `Built with Nextjs, NestJs and TypeScript, Acalainer is an online learning platform for professionals who want to automate all aspects of their educational content easily.`,
+    imgSrc: '',
+    href: 'https://acalainer.com/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Selenium based hybrid framework for testing nopcommerce platform using python',
+    description: `This framework uses the Page Object Model (POM) combined with data-driven and keyword-driven testing approaches to ensure efficiency, reusability, and maintainability. Designed specifically to automate tests for the nopCommerce website.`,
+    imgSrc: '',
+    href: 'https://github.com/alamarnissi/python-selenium-automation-framework',
+  },
+  {
+    title: 'Shopify Storefront Theme using Nextjs',
+    description: `A Next.js template powered by Tailwind CSS and TypeScript, specifically designed for Shopify. 
+    Utilizes the Shopify Storefront API through GraphQL and providing everything you need to jumpstart your Next project and save valuable time.`,
+    imgSrc: '',
+    href: 'https://github.com/alamarnissi/shop-ify-theme',
   },
 ]
 
